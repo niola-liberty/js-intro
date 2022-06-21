b9d23a590af1827ab79bb5b96b302f87a85fdb9f
niola-liberty <niolabakare@gmail.com>
2022-06-21 19:33:02 +0100

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 myInfo = ["Oluwatomisin Bakare", "5'4", "Lagos, Nigeria"];
-console.log("My name is " + myInfo[0])
-console.log("I am " + myInfo[1])
-console.log(" I live in " + myInfo[2])
+console.log("My name is " + myInfo[0]);
+console.log("I am " + myInfo[1]);
+console.log(" I live in " + myInfo[2]);
